95437a9c9d2ca0a2f6242b7e765567704e7e0e25
Linus Mårtensson <linus.martensson@gmail.com>
2014-06-22 21:23:02 +0200

    missed a spot?

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var geddy = require('geddy');
 
 geddy.startCluster({
- hostname: 'http://logistics-env-27fmmzstfv.elasticbeanstalk.com/',
+ hostname: 'logistics-env-27fmmzstfv.elasticbeanstalk.com',
  port: process.env.PORT || '3000',
  environment: process.env.NODE_ENV || 'development'
 });
