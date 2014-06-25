11943111667579f8da53897d5a5b4f258cd95d9c
Linus Mårtensson <linus.martensson@gmail.com>
2014-06-25 23:36:54 +0200

    lists again

--- app/controllers/orders.js
+++ app/controllers/orders.js
@@ -15,8 +15,8 @@ var Orders = function () {
     });
   };
 
-  this.add = function (req, resp, params) {
-	 var self = this; 
+
+	this.buildData = function(next){
 		geddy.model.Place.all(function(err, places) {
 			geddy.model.Ware.all(function(err, wares) {
 			  for(var w in wares){
@@ -27,25 +27,25 @@ var Orders = function () {
 					places[p].text = places[p].name + ", " + places[p].location;
 					places[p].value = places[p]._id;
 				}
-  			self.respond({params: params, wares:wares, places:places});
+  			next({wares:wares, places:places});
 			});
+		});
+
+	}
+
+  this.add = function (req, resp, params) {
+		var self = this; 
+		this.buildData(function(data){
+		  data.params = params;
+			self.respond(data);
 		});
   };
 
   this.create = function (req, resp, params) {
     var self = this
       , order = geddy.model.Order.create(params);
-		geddy.model.Place.all(function(err, places) {
-			geddy.model.Ware.all(function(err, wares) {
-				for(var w in wares){
-					wares[w].text = wares[w].name + ", " + wares[w].orderno + ", " + wares[w].price + ", " + wares[w].packaging + ", " + wares[w].storage;
-					wares[w].value = wares[w]._id;
-				}
-			  for(var p in places){
-					places[p].text = places[p].name + ", " + places[p].location;
-					places[p].value = places[p]._id;
-				}
-				data = {order: order, wares: wares, places: places};
+	  this.buildData(function(data){	
+				data.order = order;
  	   		if (!order.isValid()) {
  	     		this.respondWith(data);
  	   		}
@@ -57,7 +57,6 @@ var Orders = function () {
  	       		self.respondWith(data, {status: err});
  	     		});
 	    	}
-			});
 		});
   };
 
