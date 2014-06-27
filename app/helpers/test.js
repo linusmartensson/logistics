e3d0335b556dfd7d73cb81d430ae9950fc70f436
Linus Mårtensson <linus.martensson@gmail.com>
2014-06-27 23:52:14 +0200

    test

--- app/helpers/test.js
+++ app/helpers/test.js
@@ -1,4 +1,5 @@
-module.exports.Peq = function(b){
+module.exports.Peq = function(b, c){
+ if(c && this.url != b) return "";
  if((this.url).indexOf(b) == 0) return "class=\"active\"";
  return "";
 };
