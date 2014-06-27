a473aa54bd1b95ba76047e052d269806c8705873
Linus Mårtensson <linus.martensson@gmail.com>
2014-06-27 22:52:26 +0200

    test

--- app/helpers/test.js
+++ app/helpers/test.js
@@ -1,10 +1,3 @@
 module.exports.Test = function(m){
-	var s = [];
-	return JSON.stringify(m, function(k,v){
-		if(v != null && typeof v == "object"){
-			if(s.indexOf(v) >= 0) return;
-			s.push(v);
-  		}
-		return v;
- 	});
+console.dir(m);
 };
