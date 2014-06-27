31eba94fdfaf9aabb7d238c30d7d4933b39c5195
Linus Mårtensson <linus.martensson@gmail.com>
2014-06-27 22:45:26 +0200

    test

--- app/helpers/test.js
+++ app/helpers/test.js
@@ -1,3 +1,10 @@
-module.exports.Test = function(str){
-	return str.toUpperCase();
+module.exports.Test = function(m){
+	var s = [];
+	return JSON.stringify(m, function(k,v){
+		if(v != null && typeof v == "object"){
+			if(s.indexOf(v) >= 0) return;
+			s.push(v);
+  		}
+		return v;
+ 	});
 };
