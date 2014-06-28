575df02edd3df37dbd15e68d951c9d3a35faecf2
Linus Mårtensson <linus.martensson@gmail.com>
2014-06-28 16:55:04 +0200

    view updates

--- app/models/user.js
+++ app/models/user.js
@@ -1,18 +1,14 @@
 var User = function () {
   this.defineProperties({
-    username: {type: 'string', required: true, on: ['create', 'update']}
-  , password: {type: 'string', required: true, on: ['create', 'update']}
+    username: {type: 'string'}
+  , password: {type: 'string'}
   , familyName: {type: 'string', required: true}
   , givenName: {type: 'string', required: true}
-  , email: {type: 'string', required: true, on: ['create', 'update']}
+  , email: {type: 'string'}
   , activationToken: {type: 'string'}
   , activatedAt: {type: 'datetime'}
   , group: {type: 'string'}
   });
-
-  this.validatesLength('username', {min: 3});
-  this.validatesLength('password', {min: 8});
-  this.validatesConfirmed('password', 'confirmPassword');
 
   this.hasMany('Passports');
   this.hasMany('Transactions');
