7c09304736ecd97a4804b006616b5f6e1aa42a9f
Linus Mårtensson <linus.martensson@gmail.com>
2014-06-28 13:13:10 +0200

    updated table helper for action support

--- app/helpers/test.js
+++ app/helpers/test.js
@@ -4,16 +4,21 @@ module.exports.Peq = function(b, c){
  return "";
 };
 
-var tableHead = function(fields){
+var tableHead = function(fields, actions){
  ret = "";
+ actions = actions?actions:[];
 
  for(var v=0;v<fields.length;++v){
   ret += "<th>"+fields[v].text+"</th>";
  }
+ for(var v=0;v<actions.length;++v){
+  ret += "<th>"+actions[v].head?actions[v].head:"&nbsp;"+"</th>";
+ }
  return ret;
 }
-var tableBody = function(fields, values, linkPath, linkField){
+var tableBody = function(fields, values, linkPath, linkField, actions){
  ret = "";
+ actions = actions?actions:[];
  
  for(var v=0;v<values.length;++v){
   ret += "<tr>";
@@ -25,20 +30,23 @@ var tableBody = function(fields, values, linkPath, linkField){
    else
     ret += "<td>"+values[v][fields[m].field]+"</td>";
   }
+  for(var m=0;m<actions.length;++m){
+    ret += "<td>"+geddy.viewHelpers.linkTo(actions[m].text,actions[m].path(values[v][linkField]))+"</td>";
+  }
   ret += "</tr>";
  }
  return ret;
 }
 
-module.exports.buildTable = function(fields, values, linkPath, linkField){
+module.exports.buildTable = function(fields, values, linkPath, linkField, actions){
  var ret = "";
 
  ret += "<table class=\"dtables\"><thead><tr>";
- ret += tableHead(fields);
+ ret += tableHead(fields, actions);
  ret += "</tr></thead><tfoot><tr>";
  ret += tableHead(fields);
  ret += "</tr></tfoot><tbody>";
- ret += tableBody(fields, values, linkPath, linkField);
+ ret += tableBody(fields, values, linkPath, linkField, actions);
  ret += "</tbody></table>\n";
 
  return ret;
