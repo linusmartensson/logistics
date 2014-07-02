841a96b0d6db83f102404913913337045d389258
Linus Mårtensson <linus.martensson@gmail.com>
2014-07-02 21:33:02 +0200

    sums!

--- app/helpers/sums.js
+++ app/helpers/sums.js
@@ -14,6 +14,7 @@ module.exports.computeWareSums = function(wares, next){
   //Sum all transactions for each ware. Group by location type.
   for(wk=0;wk<wares.length;++wk){
    var w = wares[wk];
+   console.dir(w);
    w.groupsums = {};
    w.placesums = {};
    for(tk=0;tk<transactions.length;++tk){
