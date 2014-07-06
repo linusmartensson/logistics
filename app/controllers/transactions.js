55e71d9f4ec156cfd9facca999a865d6b626de54
Linus Mårtensson <linus.martensson@gmail.com>
2014-07-06 17:53:31 +0200

    transaction editing

--- app/controllers/transactions.js
+++ app/controllers/transactions.js
@@ -1,76 +1,66 @@
 var Transactions = function () {
   this.respondsWith = ['html', 'json', 'xml', 'js', 'txt'];
 
-	this.buildData = function(next){
-		geddy.model.Place.all(function(err, places) {
-			geddy.model.Ware.all(function(err, wares) {
-			  for(var w in wares){
-					wares[w].text = wares[w].name + ", " + wares[w].orderno + ", " + wares[w].price + ", " + wares[w].packaging + ", " + wares[w].storage;
-					wares[w].value = wares[w].id;
-				}
-			  for(var p in places){
-					places[p].text = places[p].name + ", " + places[p].location;
-					places[p].value = places[p].id;
-				}
-  			next({wares:wares, places:places});
-			});
-		});
-	};
+  this.buildData = function(next){
+   geddy.model.Place.all(function(err, places) {
+    geddy.model.Ware.all(function(err, wares) {
+     for(var w in wares){
+      wares[w].text = wares[w].name + ", " + wares[w].orderno + ", " + wares[w].price + ", " + wares[w].packaging + ", " + wares[w].storage;
+      wares[w].value = wares[w].id;
+     }
+     for(var p in places){
+      places[p].text = places[p].name + ", " + places[p].location;
+      places[p].value = places[p].id;
+     }
+     next({wares:wares, places:places});
+    });
+   });
+  };
 
   this.index = function (req, resp, params) {
-    var self = this;
-
-		q = {};
-		o = {sort: {createdAt: 'asc'}, includes: ['places', 'wares']};
-    geddy.model.Transaction.all(q,o, function(err, transactions) {
-      if (err) {
-        throw err;
-      }
-      self.respondWith(transactions, {type:'Transaction'});
-    });
+   var self = this;
+   q = {};
+   o = {sort: {createdAt: 'asc'}, includes: ['places', 'wares']};
+   geddy.model.Transaction.all(q,o, function(err, transactions) {
+    if (err) throw err;
+    self.respondWith(transactions, {type:'Transaction'});
+   });
   };
 
   this.add = function (req, resp, params) {
-    var self = this;
-    this.buildData(function(data){
-      data.params = params;
-      self.respond(data);
-    });
+   var self = this;
+   this.buildData(function(data){
+    data.params = params;
+    self.respond(data);
+   });
   };
 
   this.create = function (req, resp, params) {
-    var self = this;
+   var self = this;
+   var pfrom = {wareId:params.wareId, count:-params.count, placeId:params.fromLocation}
+     , pto = {wareId:params.wareId, count:params.count, placeId:params.toLocation};
+   var transactionFrom = geddy.model.Transaction.create(pfrom)
+     , transactionTo = geddy.model.Transaction.create(pto);
 
-    var pfrom = {wareId:params.wareId, count:-params.count, placeId:params.fromLocation}
-      , pto = {wareId:params.wareId, count:params.count, placeId:params.toLocation};
-
-    var transactionFrom = geddy.model.Transaction.create(pfrom)
-      , transactionTo = geddy.model.Transaction.create(pto);
-
-
-    this.buildData(function(data){
-      if (!transactionFrom.isValid() || !transactionTo.isValid()) {
-         
-        self.respond(params);
-      }
-      else {
-        transactionFrom.save(function(err, data) {
-          if (err) {
-            throw err;
-          }
-          transactionTo.save(function(err1, data1) {
-    	    console.dir(transactionFrom);
-            console.dir(transactionTo);
-            if (err1) {
-              geddy.model.Transaction.remove(transactionFrom.id, function(err2, data2){
-                throw err1;
-              });
-            }
-            self.respondWith(transactionTo, {status: err1});
-          });
-        });
-      }
-    });
+   this.buildData(function(data){
+    if (!transactionFrom.isValid() || !transactionTo.isValid()) self.respond(params);
+    else {
+     transactionFrom.save(function(err, data) {
+      if (err) throw err;
+      transactionTo.updateProperties({pairTransactionId:transactionFrom.id});
+      transactionTo.save(function(err1, data1) {
+       transactionFrom.updateProperties({pairTransactionId:transactionTo.id});
+       transactionFrom.save(function(err3, data3) {
+       if (err1||err3){
+         geddy.model.Transaction.remove(transactionFrom.id, function(err2, data2){throw err1||err3||err2;});
+         geddy.model.Transaction.remove(transactionTo.id, function(err2, data2){throw err1||err3||err2;});
+        }
+        self.respondWith(transactionTo, {status: err1});
+       }
+      });
+     });
+    }
+   });
   };
 
   this.show = function (req, resp, params) {
@@ -94,16 +84,21 @@ var Transactions = function () {
   this.edit = function (req, resp, params) {
     var self = this;
 
-    geddy.model.Transaction.first(params.id, function(err, transaction) {
-      if (err) {
-        throw err;
-      }
-      if (!transaction) {
-        throw new geddy.errors.BadRequestError();
-      }
-      else {
-        self.respondWith(transaction);
-      }
+    geddy.model.Transaction.first(params.id, function(err, transactionA) {
+     geddy.model.Transaction.first(transactionA.pairTransactionId, function(err, transactionB) {
+      self.buildData(function(data){
+       if (err) throw err;
+       if (!transactionA||!transactionB) throw new geddy.errors.BadRequestError();
+       else {
+        data.transaction = {};
+        data.transaction.fromLocation = transactionA.count<0?transactionA.placeId:transactionB.placeId;
+        data.transaction.toLocation = transactionA.count>0?transactionA.placeId:transactionB.placeId;
+        data.transaction.count = transactionA.count;
+        data.transaction.wares = transactionA.wareId;
+        self.respond(data);
+       }
+      });
+     }
     });
   };
 
@@ -150,7 +145,6 @@ var Transactions = function () {
       }
     });
   };
-
 };
 
 exports.Transactions = Transactions;
