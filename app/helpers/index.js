21e20bd94c86de94af625027c9e64fc7003a9d91
Linus Mårtensson <linus.martensson@gmail.com>
2014-07-13 13:53:35 +0200

    month bug

---
+++ app/helpers/index.js
@@ -0,0 +1,24 @@
+
+exports.requireGroup = function (group) {
+ var sgroup = this.session.get('userGroup');
+ if(!sgroup || (group instanceof String && sgroup != group)) this.redirect('/');
+ else {
+  for(var v=0;v<group.length;++v){
+   if(sgroup == group[v]) return; 
+  }
+  this.redirect('/');
+ }
+}
+
+exports.checkGroup = function (group) {
+ var sgroup = this.session.get('userGroup');
+ if(!sgroup || (group instanceof String && sgroup != group)) return false;
+ else {
+  for(var v=0;v<group.length;++v){
+   if(sgroup == group[v]) return true; 
+  }
+  return false;
+ }
+ return true;
+}
+
